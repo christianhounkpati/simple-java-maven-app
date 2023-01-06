34efb2c0be822efc2a58bb95b539e4fb33bf05ab
christianhounkpati <121950926+christianhounkpati@users.noreply.github.com>
2023-01-06 11:46:31 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! It's Java maven";
+    private static final String MESSAGE = "Hello World! It is Java maven";
 
     public App() {}
 
